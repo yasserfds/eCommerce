c7a797a3441acfd4cfb9227f03c3d7e65b3462c9
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-23 11:26:38 +0100

    Fixing some bugs

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Soon
+// Cart
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+// When cart is active
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+}
+
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+}
