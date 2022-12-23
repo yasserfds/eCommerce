e817e11fc47211152cf09c0f5cf6e89f96c38cac
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-23 12:45:37 +0100

    Working on addProduct to cart function

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ closeCart.onclick = () => {
 
 // Cart working JS
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+    document.addEventListener('DOMContentLoaded', ready);
 } else {
     ready();
 };
@@ -29,12 +29,62 @@ function ready() {
         var button = removeCartButtons[i];
         button.addEventListener('click', removeCartItem);
     };
+
+    // Quantity changes
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change', quatityChanged);
+    };
+
+    // Add To Cart
+    var addCart = document.getElementsByClassName('add-cart')
+    for (var i = 0; i < addCart.length; i++) {
+        var button = addCart[i];
+        button.addEventListener('click', addCartClicked);
+    };
 };
 
 // Remove Items From Cart
 function removeCartItem(event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+    updateTotal();
+};
+
+// Quantity Changes
+function quatityChanged(event) {
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    } else {
+        if(isNaN(input.value) || input.value >= 100) {
+            input.value = 1;
+        }
+    }
+    updateTotal();
+};
+
+// Add To Cart
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+    var price = shopProducts.getElementsByClassName('price')[0].innerText;
+    var productImg = shopProducts.getElementsByClassName('product-img')[0].src;
+    console.log(title, price, productImg);
+    updateTotal();
+}
+
+// Add Product To Cart
+function addProductToCart(title, price, productImg) {
+    var cartShopBox = document.createElement('div');
+    // cartShopBox.classList.add('cart-box');
+    var cartItems = document.getElementsByClassName('cart-content')[0];
+    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        alert("You have already add this item to cart");
+    };
 };
 
 // Update Total
@@ -42,12 +92,17 @@ function updateTotal() {
     var cartContent = document.getElementsByClassName('cart-content') [0];
     var cartBoxes = cartContent.getElementsByClassName('cart-box');
     var total = 0;
-    for(var i = 0; i < cartBoxes.length; i++) {
+    for (var i = 0; i < cartBoxes.length; i++) {
         var cartBox = cartBoxes[i]
         var priceElement = cartBox.getElementsByClassName('cart-price') [0];
         var quantityElement = cartBox.getElementsByClassName('cart-quantity') [0];
         var price = parseFloat(priceElement.innerText.replace("$", ""));
         var quantity = quantityElement.value;
-        // total = total + price * quantity;
+        total = total + (price * quantity);
+
+        // If Price Containes some Cents value
+        total = Math.round(total * 100) / 100;
+
+        document.getElementsByClassName('total-price')[0].innerText = "$" + total;
     }
 }
