979c93e6bffeda17561d38dff58aafedc2d5768c
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-23 11:47:31 +0100

    Working on updateTotal func

--- main.js
+++ main.js
@@ -6,8 +6,48 @@ let closeCart = document.querySelector('#close-cart');
 // When cart is active
 cartIcon.onclick = () => {
     cart.classList.add("active");
-}
+};
 
+// When you close the cart
 closeCart.onclick = () => {
     cart.classList.remove("active");
+};
+
+// Cart working JS
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready();
+};
+
+// Making functions
+function ready() {
+    // Remove Items From Cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove');
+    console.log(removeCartButtons);
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    };
+};
+
+// Remove Items From Cart
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+};
+
+// Update Total
+function updateTotal() {
+    var cartContent = document.getElementsByClassName('cart-content') [0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var total = 0;
+    for(var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i]
+        var priceElement = cartBox.getElementsByClassName('cart-price') [0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity') [0];
+        var price = parseFloat(priceElement.innerText.replace("$", ""));
+        var quantity = quantityElement.value;
+        // total = total + price * quantity;
+    }
 }
